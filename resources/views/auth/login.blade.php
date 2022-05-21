f3ab5380c6be89e9d12f42e0a9a06cc13e7cce54
Faiz Dzakirullail <102015243+Faiz1410@users.noreply.github.com>
2022-05-21 22:44:46 +0700

    Add some routes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,18 @@
-@extends('layouts.template')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<div class="logo d-inline-block text-center text-light"><img src="/assets/logo.png"> HMIF</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!--custom css-->
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="logo d-inline-block text-center text-light"><img src="/assets/logo.png"> HMIF</div>
     <div class="global-container">
         <div class="card login-form">
             <div class="card-body">
@@ -27,3 +38,71 @@
             </div>
         </div>
     </div>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</body>
+<style>
+    html, body{
+    height:100%;
+    background-color: #2F303A;
+}
+.logo{
+    position: absolute;
+    height:100px;
+    width:100px;
+    padding-top: 10px;
+    padding-left: 10px;
+}
+
+.global-container{
+    height:100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color:#FFF;
+}
+
+.login-form{
+    height: 400px;
+    width: 450px;
+    padding-left: 30px;
+    padding-right: 30px;
+    background-color: #2F303D !important;
+    border-radius: 10px;
+}
+
+input[type="text"],
+input[type="password"]{
+    background: #2f303d;
+    color: #FFF;
+    border:2px solid #FFF;
+    border-radius: 10px;
+    margin-bottom: 5px;
+}
+
+input[type="text"]:focus,
+input[type="password"]:focus{
+    outline: none;
+    border: none;
+    background: #2F303C;
+    color: #FFF;
+    margin: 0;
+}
+
+.judul-login{
+    font-weight: 700;
+    padding-top: 25px;
+}
+
+.btn{
+    color: #000 !important;
+    transform: translateY(10px);
+    font-size: 14px;
+    border-radius: 10px !important;
+    font-weight: 700;
+}
+</style>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome',[
+        "title" => "Welcome"
+    ]);
+});
+
+Route::get('/login', function () {
+    return view('auth.login',[
+        "title" => "Login"
+    ]);
+});
+
+Route::get('/beranda', function () {
+    return view('pages.beranda',[
+        "title" => "Beranda"
+    ]);
 });
