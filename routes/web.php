2805d4cc5fc4c6fa30860afe4c2a7a810bcd63fb
yulkhains <yulia_kh@mhs.unsyiah.ac.id>
2022-05-21 14:24:42 +0700

    add beberapa fitur

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/data_peserta', function () {
+    return view('data_peserta');
+});
+
+Route::get('/detai', function () {
+    return view('detail');
+});
+
+Route::get('/hapus', function () {
+    return view('hapus');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/departemen', function () {
+    return view('departemen');
+});
